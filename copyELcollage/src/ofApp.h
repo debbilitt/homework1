f761bc2ddc495d11f656a6d1f48e8c3ba4c31ddb
Emeline Liu <etl12@case.edu>
2018-02-04 14:11:13 -0500

    update copy folder

--- copyELcollage/src/ofApp.h
+++ copyELcollage/src/ofApp.h
@@ -3,30 +3,30 @@
 #include "ofMain.h"
 
 class ofApp : public ofBaseApp{
-    
-public:
-    void setup();
-    void update();
-    void draw();
-    
-    void keyPressed(int key);
-    void keyReleased(int key);
-    void mouseMoved(int x, int y );
-    void mouseDragged(int x, int y, int button);
-    void mousePressed(int x, int y, int button);
-    void mouseReleased(int x, int y, int button);
-    void mouseEntered(int x, int y);
-    void mouseExited(int x, int y);
-    void windowResized(int w, int h);
-    void dragEvent(ofDragInfo dragInfo);
-    void gotMessage(ofMessage msg);
-    
-    ofImage mCat;
-    int offset[50];
-    int size[50];
-    int sourceX[50];
-    int sourceY[50];
-    int dist[50];
-    int centX;
-    int centY;
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		
+		ofImage mCat;
+		int offset[50];
+		int size[50];
+		int sourceX[50];
+		int sourceY[50];
+		int dist[50];
+		int centX;
+		int centY;
 };
